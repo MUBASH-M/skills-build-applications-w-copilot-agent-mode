bcf3fd6b64500af778b7e0630801fbce74e896b7
MUBASH M <42893803+MUBASH-M@users.noreply.github.com>
2025-04-08 09:09:43 +0000

    Commit changes

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_7p#1z=zngk+tktsoh-&w21qp@8)1$dmrw88*)x$#htoud()pv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'congenial-xylophone-rpgxxxwr9jwh5rp7-8000.app.github.dev']
 
 
 # Application definition
